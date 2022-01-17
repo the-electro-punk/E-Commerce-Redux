2236e2084c175eea7bb6cda120c1fe23546c6485
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2022-01-16 20:02:50 -0600

    updated server and app

--- Develop/server.js
+++ Develop/server.js
@@ -1,55 +1,93 @@
-const express = require('express');
-const routes = require('./routes');
-const mysql = require('mysql');
-// const { sequelize } = require('./models/Product');
-const sequelize = require('./config/connection')
+// these functions should adapt the original file to a redux connection
 
-// this links to list of products script
-// let products = require('./seeds/product-seeds.js')
+// var path = require('path')
+// var Express = require('express')
+// var React = require('react')
+// var { createStore } = require('redux')
+// var { Provider } = require('react-redux')
+// var counterApp = require('./reducers')
+// var App = require('./containers/App')
+// var {renderToString} = require('react-dom/server')
+import qs from 'qs'
+import path from 'path'
+import Express from 'express'
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import counterApp from './reducers'
+import App from './containers/App'
 
-// this connects the JS code to the database
-// var connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'password',
-//   // database: 'ecommerce_db',
-//   // port: '3001',
-// })
+const app = Express()
+const port = 4000
 
-// // this connects to mysql and creates a database
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   console.log('connected');
-//   connection.query("CREATE DATABASE ecommerce_db", function(err, result) {
-//     if (err) throw err;
-//     console.log('database created');
-//   })
-// })
+app.use('/static', Express.static('static'))
 
-// this was code from a previous challenge in an attempt to insert data into the table
-// connection.query(`INSERT INTO ${tableName}(ID, Employee, Position, Department) VALUES (${empID},'${nameEmp}','${position}','${department}')`, (err,rows) => {
-//   if(err) {
-//       throw err
-//   }
-//   else {
-//       console.log('data inserted')
-//       console.log(rows)
-//   }
-// })
+app.use(handleRender)
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+function handleRender(req, res) {
+    const store = createStore(counterApp)
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+    const html = renderToString(
+        // store={store}
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+    const preloadedState = store.getState()
 
-app.use(routes);
+    res.send(renderFullPage(html, preloadedState))
+}
 
-// sync sequelize models to the database, then turn on the server
-sequelize.sync({force:false}).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });  
-})
+function renderFullPage(html, preloadedState) {
+    return `
+    <!doctype html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>WeatherForecast</title>
+        <link rel="stylesheet" href=".\assets\css\Weather.css">
+    </head>
+    <body>
+        <header class="header">{${html}}</header>
+        
+        <div class="content">
+            <div class="search">
+                <h2>Enter a city</h2>
+                <input type="text" id="searchBar" class="searchBar">
+                <button id="searchBTN" class="searchBTN">Search</button>
+                <div id="citySearch" class="citySearch"></div>
+            </div>
 
-// be able to connect to a database, download seed data, then input data???
+            <div class="citySelect">
+                <img class="cityPhoto" src="">
+                <select id="citiesChoice" class="cities">
+                    <option value="Nashville">Nashville</option>
+                    <option value="Knoxville">Knoxville</option>
+                    <option value="Memphis">Memphis</option>
+                    <option value="Chattanooga">Chattanooga</option>
+                    <option value="Murfreesboro">Murfreesboro</option>
+                    <option value="Clarksville">Clarksville</option>
+                </select>
+                <button id="searchBTN" class="searchBTN">Search</button>
+            </div>
+
+            <div class="weather">
+                <div id="cityData" class="cityData" >City Data</div>
+                <div class="fiveDays">
+                    <div id="dayOne" class="day"></div>
+                    <div id="dayTwo" class="day"></div>
+                    <div id="dayThree" class="day"></div>
+                    <div id="dayFour" class="day"></div>
+                    <div id="dayFive" class="day"></div>
+                </div>
+            </div>
+        </div>   
+    </body>
+    <script>window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace}</script>
+    `
+}
+
+app.listen(port)
+
+// this link helped me out https://redux.js.org/usage/server-rendering
